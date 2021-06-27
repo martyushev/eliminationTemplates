d0ad7a0b58b94135a5a0cf2fe75f8dc433f49c52
martyushev <72862703+martyushev@users.noreply.github.com>
2021-06-27 20:48:15 +0500

    GBsparseLA

---
+++ problems/F_wpnp_2x2sym.m
@@ -0,0 +1,6 @@
+M7R0
+I.F_wpnp_2x2sym7&,,*&%"xG"""%"zG""#"&K%=*&F&F'%"wGF)"&o"R*&^$"%GH"%7")F'F&F'F'*
+(%"yGF'F(F'F,F'"%;#**&^$!%?V"%3YF'F3F'F',,*(F&F'F(F'F,F'F4*&F6F'F&F'F'*&F3F'F(F
+)F-*&F3F'F,F)F**&^$"&sQ""%oVF'F3F'F',,*&F&F)F(F'F**(F&F'F3F'F,F'F4*&F3F)F(F'F-*
+&^$F@!%oVF'F(F'F'*&^$F7!%3YF'F,F'F',,*&F&F)F,F'F-*(F&F'F3F'F(F'F4*&F3F)F,F'F**&
+FJF'F(F'F'*&^$F0!%7")F'F,F'F'6"
