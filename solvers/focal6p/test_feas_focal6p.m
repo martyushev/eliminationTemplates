1e5f8af8690ab3567f0e8597647ad154ada10252
martyushev <72862703+martyushev@users.noreply.github.com>
2022-01-20 16:16:46 +0500

    Matlab solvers

--- solvers/focal6p/test_feas_focal6p.m
+++ solvers/focal6p/test_feas_focal6p.m
@@ -28,3 +28,21 @@ folder = fileparts(which('add_all.m'));
 save(strcat(folder,'\_results\Err_feas_focal6p.mat'),'Err_feas_focal6p');
 
 fprintf('Problem #3. Med. error in f: %0.2e. Mean error in f: %0.2e. Number of fails: %0.2e\n',median(Err_feas_focal6p),mean(Err_feas_focal6p),(N-length(Err_feas_focal6p))*100/N);
+
+
+
+function [F,E] = esse_focal6p(ff,uu,vv,M)
+
+    n = length(ff);
+    E = zeros(9,n);
+    ns = [-M;eye(3)];
+    F = ns(:,3)*ones(1,n) + ns(:,2)*uu + ns(:,1)*vv;
+
+    for i = 1:n
+        K = diag([ff(i) ff(i) 1]);
+        F(:,i) = F(:,i)/norm(F(:,i),'fro');
+        E(:,i) = reshape(K*reshape(F(:,i),3,3)*K,9,1);
+        E(:,i) = E(:,i)/norm(E(:,i),'fro');
+    end
+
+end
--- solvers/focal6p_1s/test_feas_focal6p_1s.m
+++ solvers/focal6p_1s/test_feas_focal6p_1s.m
@@ -28,3 +28,21 @@ folder = fileparts(which('add_all.m'));
 save(strcat(folder,'\_results\Err_feas_focal6p_1s.mat'),'Err_feas_focal6p_1s');
 
 fprintf('Problem #2. Med. error in f: %0.2e. Mean error in f: %0.2e. Number of fails: %0.2e\n',median(Err_feas_focal6p_1s),mean(Err_feas_focal6p_1s),(N-length(Err_feas_focal6p_1s))*100/N);
+
+
+
+function [F,E] = esse_focal6p_1s(ff,uu,vv,M)
+
+    n = length(ff);
+    E = zeros(9,n);
+    ns = [-M;eye(3)];
+    F = ns(:,3)*ones(1,n) + ns(:,2)*uu + ns(:,1)*vv;
+
+    for i = 1:n
+        K = diag([ff(i) ff(i) 1]);
+        F(:,i) = F(:,i)/norm(F(:,i),'fro');
+        E(:,i) = reshape(K*reshape(F(:,i),3,3),9,1);
+        E(:,i) = E(:,i)/norm(E(:,i),'fro');
+    end
+
+end
