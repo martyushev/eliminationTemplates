4b2835135d274b4e620a86130b8f40d4cddc0242
martyushev <72862703+martyushev@users.noreply.github.com>
2022-01-22 22:21:01 +0500

    GreedyAG

---
+++ GreedyAG/problems/F_pose_quiver.m
@@ -0,0 +1,8 @@
+M7R0
+I.F_pose_quiver7&,:#!'s4?"'d&[)"""*&%"xG""#%"wGF(F(*&%"yGF+F,F(F(*&%"zGF+F,F(!"
+"F,F1*&F*F(F.F(#"(i`X$F'*&F*F(F0F(#"(wT)=F'F*#!(I)fDF'*&F.F(F0F(#"(I)fDF'F.F6*$
+F0F+#"'s4?F'F0#!(QZ%HF',6#"'sTzF'F(*(F*F(F0F(F,F(F(*&F.F(F,F(F1F2#"("[v;"(rca#F
+5#"'1ppF'F*#!'M=eFIF:#"'M=eFIF.FJF=#!'sTzF'F0#!'haTF',6#!&K6*F'F(*&F*F(F,F(F(*(
+F.F(F0F(F,F(F(F2#"(X."\FIF5#"'=:YF'F*#!(/-'HFIF:#"(/-'HFIF.FenF=#"&K6*F'F0#!'H`
+^F',6#!(V0."F'F(*$F*F+F(F2#"(uU="F'F5#!''4*pF'F*#!'uiUF'*$F.F+F1F:#"'uiUF'F.Feo
+F=#"(V0."F'F0#"'1.HF'6"
