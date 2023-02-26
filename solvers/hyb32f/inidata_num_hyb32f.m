5a4ea656a7ef2cc65d1c7851fc77c1149a0f9373
martyushev <72862703+martyushev@users.noreply.github.com>
2023-02-26 12:41:20 +0500

    upd26022023

---
+++ solvers/hyb32f/inidata_num_hyb32f.m
@@ -0,0 +1,4 @@
+function data = inidata_num_hyb32f()
+    data = cell([1,1]);
+    data{1} = randn(23,1);
+end
---
+++ solvers/hyb32f/test_num_hyb32f.m
@@ -0,0 +1,29 @@
+rng(23);
+N = 1;
+d = 28;
+
+stats = struct('problem','hyb32f','tm',[],'err',[],'k',[],'kr',[],'N',N);
+
+for i = 1:N
+
+    data = inidata_num_hyb32f(); % generate initial data
+
+    try
+        C = coefs_hyb32f(data); % compute coefficients of polynomial system
+        tic;
+        S = red_174x222_hyb32f(C); % solve polynomial system
+        stats.tm = [stats.tm toc];
+        if isempty(S); continue; end
+    catch ME
+        continue;
+    end
+
+    mon = @(r1,r2,t3) [r1^9*t3,r1^8*t3*r2,r1^7*t3*r2^2,r1^6*t3*r2^3,r1^5*t3*r2^4,r1^4*t3*r2^5,r1^3*t3*r2^6,r1^2*r2^7*t3,r1*r2^8*t3,r2^9*t3,r1^9,r1^8*r2,r1^7*r2^2,r1^6*r2^3,r1^5*r2^4,r1^4*r2^5,r1^3*r2^6,r1^2*r2^7,r1*r2^8,r2^9,r1^8*t3,r1^7*t3*r2,r1^6*t3*r2^2,r1^5*t3*r2^3,r1^4*t3*r2^4,r1^3*t3*r2^5,r1^2*t3*r2^6,r1*r2^7*t3,r2^8*t3,r1^8,r1^7*r2,r1^6*r2^2,r1^5*r2^3,r1^4*r2^4,r1^3*r2^5,r1^2*r2^6,r1*r2^7,r2^8,r1^7*t3,r1^6*t3*r2,r1^5*t3*r2^2,r1^4*t3*r2^3,r1^3*t3*r2^4,r1^2*t3*r2^5,r1*t3*r2^6,r2^7*t3,t3^2*r1^6,t3^2*r1^4*r2^2,t3^2*r1^2*r2^4,t3^2*r2^6,r1^7,r1^6*r2,r1^5*r2^2,r1^4*r2^3,r1^3*r2^4,r1^2*r2^5,r1*r2^6,r2^7,t3*r1^6,r1^5*t3*r2,t3*r1^4*r2^2,r1^3*t3*r2^3,t3*r1^2*r2^4,r1*t3*r2^5,t3*r2^6,t3^2*r1^5,t3^2*r1^4*r2,t3^2*r1^3*r2^2,t3^2*r1^2*r2^3,t3^2*r1*r2^4,t3^2*r2^5,r1^6,r1^5*r2,r1^4*r2^2,r1^3*r2^3,r1^2*r2^4,r1*r2^5,r2^6,t3*r1^5,t3*r1^4*r2,t3*r1^3*r2^2,t3*r1^2*r2^3,t3*r1*r2^4,t3*r2^5,t3^2*r1^4,t3^2*r1^2*r2^2,t3^2*r2^4,r1^5,r1^4*r2,r1^3*r2^2,r1^2*r2^3,r1*r2^4,r2^5,t3*r1^4,r1^3*t3*r2,t3*r1^2*r2^2,r1*t3*r2^3,t3*r2^4,t3^2*r1^3,t3^2*r1^2*r2,t3^2*r1*r2^2,t3^2*r2^3,r1^4,r1^3*r2,r1^2*r2^2,r1*r2^3,r2^4,t3*r1^3,t3*r1^2*r2,t3*r1*r2^2,t3*r2^3,t3^2*r1^2,t3^2*r2^2,r1^3,r1^2*r2,r1*r2^2,r2^3,t3*r1^2,r1*t3*r2,t3*r2^2,t3^2*r1,t3^2*r2,r1^2,r1*r2,r2^2,t3*r1,t3*r2,t3^2,r1,r2,t3,1];
+    [err,k,kr] = numerr(C,mon,S,d); % compute numerical error
+    stats.err = [stats.err err];
+    stats.k = [stats.k k];
+    stats.kr = [stats.kr kr];
+
+end
+
+disp_stats(stats,1);
